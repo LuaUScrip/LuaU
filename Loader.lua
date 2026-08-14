dd6f9cfe727dbe03637b82785dcbc1df4054d986
LuaUScrip <inikaptensiwa@gmail.com>
2026-08-14 15:27:48 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -46,7 +46,7 @@ local SupportedGames = {
 	{name = "+1 Backflip Keyboard Escape", placeIds = {77764718018334}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/Plus1BackflipKeyboardEscapeW2.lua"},
 	{name = "Web Swing For Lucky Blocks", placeIds = {86455914125987}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/WebSwingForLuckyBlock.lua"},
 	{name = "+1 Fire Per Click", placeIds = {70970180473702}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/Plus1FirePerClick.lua"},
-	{name = "Be A Fish Bait", placeIds = {0}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/BeAFishBait.lua},
+	{name = "Be A Fish Bait", placeIds = {99702578544768}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/BeAFishBait.lua"},
 	{name = "Script 38", placeIds = {0}, url = nil},
 	{name = "Script 39", placeIds = {0}, url = nil},
 	{name = "Script 40", placeIds = {0}, url = nil},
