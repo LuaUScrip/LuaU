aafc3302f1f72bec32f362a0e76a4c5c106cf10e
LuaUScrip <inikaptensiwa@gmail.com>
2026-08-19 00:04:00 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -53,7 +53,7 @@ local SupportedGames = {
 	{name = "+1 Roller For Needoh ", placeIds = {81700526542690}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/Plus1RollerForNeedoh.lua"},
 	{name = "+1 High Jump Power Escape", placeIds = {83388257263676}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/Plus1HighJumpPowerEscape.lua"},
 	{name = "Murderers VS Sheriffs", placeIds = {12355337193}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/MurderVsSherif.lua"},
-	{name = "+1 Web Swing Escape", placeIds = {110668201954727}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/WebSwingEscape lua"},
+	{name = "+1 Web Swing Escape", placeIds = {110668201954727}, url = "https://raw.githubusercontent.com/LuaUScrip/LuaU/refs/heads/main/WebSwingEscape.lua"},
 	{name = "Script 45", placeIds = {0}, url = nil},
 	{name = "Script 46", placeIds = {0}, url = nil},
 	{name = "Script 47", placeIds = {0}, url = nil},
